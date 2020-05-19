788771a993c0337a6209cb1565232b82fd0a969c
AbrahamCain <54870658+AbrahamCain@users.noreply.github.com>
2020-05-19 17:48:15 -0400

    Create Hello.java
    A simple java program to print "Hello World!" to the command line.

---
+++ Hello.java
@@ -0,0 +1,15 @@
+//This is a simple java program that will need to be compiled before it can be run so that the Hello.class can be created
+//It can be compiled from windows CMD.exe with the command "javac Hello.java"
+
+public class Hello {
+
+	public static void main(String[] args) {
+  
+    //prints out "Hello World!" to the console
+		System.out.println("Hello World!"); 
+    
+	}
+}
+
+
+//Note to self: semicolons must be present at the end of every command otherwise an error will occur
