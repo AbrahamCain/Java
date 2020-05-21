95e8c1cd2bbed77012f3b4ad66d59fb3f502f30e
AbrahamCain <54870658+AbrahamCain@users.noreply.github.com>
2020-05-21 01:10:10 -0400

    Update MathMagic.java

--- MathMagic.java
+++ MathMagic.java
@@ -1,17 +1,17 @@
 public class MathMagic {
 	public static void main(String[] args) {
-    //Change this input number to any almost any other int number and you will get the same result 
-    int myNumber = 24;
+		//Change this input number to any almost any other int number and you will get the same result 
+		int myNumber = 24;
 
-	  int stepOne = myNumber * myNumber;
-    int stepTwo = stepOne + myNumber;
-    int stepThree = stepTwo / myNumber;
-    int stepFour = stepThree + 17;
-    int stepFive = stepFour - myNumber;
-    int stepSix = stepFive / 6;
+		int stepOne = myNumber * myNumber;
+	        int stepTwo = stepOne + myNumber;
+	        int stepThree = stepTwo / myNumber;
+	        int stepFour = stepThree + 17;
+	        int stepFive = stepFour - myNumber;
+	        int stepSix = stepFive / 6;
 
-    //print final value which is almost always 3
-    System.out.println(stepSix);
+	        //print final value which is almost always 3
+	        System.out.println(stepSix);
 
 	}
 }
